14edd7c09efbb5a1ab24faf46fcaff9a8617ffc0
PranjaliBhardwaj027 <154813094+PranjaliBhardwaj027@users.noreply.github.com>
2024-02-05 20:07:27 +0530

    Create Q2.cpp

---
+++ PatternQues/Q2.cpp
@@ -0,0 +1,26 @@
+/*    1
+      2 2
+      3 3 3
+      4 4 4 4     */
+void triangle(int n) {
+	for(int i=1; i<=n; i++) {
+for(int j=1; j<=i; j++) {
+cout<<i<<" ";
+}
+cout<<endl;
+}
+
+}
+
+/*    * * * *
+      * *
+      *
+          */
+void seeding(int n) {
+	for(int i=1; i<=n; i++) {
+for(int j=1; j<=n-i+1; j++) {
+cout<<"* ";
+}
+cout<<endl;
+}
+}
